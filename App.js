eb48df04ff02a13cd0fb050e98c1aa9b7c4834b8
millifail <hwangbird99@gmail.com>
2023-10-13 17:32:24 +0900

    초기 세팅 및 라우팅

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/Home';
+import Search from './src/screens/Search';
+import Activity from './src/screens/Activity';
+import Profile from './src/screens/Profile';
+import Status from './src/screens/Status';
+import FriendProfile from './src/screens/FriendProfile';
+import EditProfile from './src/screens/EditProfile';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+  const BottomTabScreen = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarHideOnKeyboard: true,
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            height: 70,
+          },
+        })}
+      >
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Search' component={Search} />
+        <Tab.Screen name='Activity' component={Activity} />
+        <Tab.Screen name='Profile' component={Profile} />
+      </Tab.Navigator>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Bottom' component={BottomTabScreen} />
+        <Stack.Screen name='Status' component={Status} />
+        <Stack.Screen name='FriendProfile' component={FriendProfile} />
+        <Stack.Screen name='EditProfile' component={EditProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/screens/FriendProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const FriendProfile = () => {
+  return (
+    <View>
+      <Text>FriendProfile</Text>
+    </View>
+  );
+};
+
+export default FriendProfile;
+
+const styles = StyleSheet.create({});
